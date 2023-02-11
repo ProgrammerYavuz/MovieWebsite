74a6419176f715a3679dbb0450042b47b669c9d0
Yavuz <yavuzsocialmedia@gmail.com>
2023-02-11 14:33:22 +0300

    header field created

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+        lg: '1140px',
+        xl: '1140px',
+        '2xl': '1140px',
+      }
+    },
+    extend: {
+      fontFamily: {
+        gemunu: ['Gemunu Libre', 'sans-serif'],
+        open: ['Open Sans', 'sans-serif']
+      },
+      colors: {
+        'movie-red'   : '#bc1a45',
+        'movie-melon' : '#ffd369',
+        'movie-grey'  : '#dddddd',
+        'movie-white' : '#f7f7f7',
+      }
+    },
   },
   plugins: [],
 }
